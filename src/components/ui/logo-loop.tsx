46531c31ddea1c29bf132023cee7bda382c2a915
ECX Developers <dev.encryptarx@gmail.com>
2025-10-08 12:50:25 +0000

    no use exactly like this codes:
    npx shadcn@latest add https://reactbit

--- src/components/ui/logo-loop.tsx
+++ src/components/ui/logo-loop.tsx
@@ -254,7 +254,7 @@ const LogoLoop = React.memo<LogoLoopProps>(
           '[--logoloop-gap:32px]',
           '[--logoloop-logoHeight:28px]',
           '[--logoloop-fadeColorAuto:#ffffff]',
-          'dark:bg-surface-2',
+          'dark:[--logoloop-fadeColorAuto:#0b0b0b]',
           scaleOnHover && 'py-[calc(var(--logoloop-logoHeight)*0.1)]',
           className
         ),
